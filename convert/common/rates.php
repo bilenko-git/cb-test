f09ef24ca193a5257bbfb57e6cd904635f0ea6f0
Dima Zabrodin <zabrodin17081990@gmail.com>
2015-08-26 19:14:23 +0300

    HOTELS-9974 add rate

--- convert/common/rates.php
+++ convert/common/rates.php
@@ -1,9 +1,61 @@
 <?php
 
 trait Rates {
-    private $rates_url = 'http://{server}/connect/{property_id}#/roomRates';
+    private $roomRate_url = 'http://{server}/connect/{property_id}#/roomRates';
+    private $reservas_url = 'http://{server}/reservas/{property_id}';
+    private $availability_url = 'http://{server}/connect/{property_id}#/availability';
 
-    public function rates_create_rate($rate) {
-        return false;
+    public function rates_add_rate($interval) {
+        $this->url($this->_prepareUrl($this->roomRate_url));
+        $this->waitForLocation($this->_prepareUrl($this->roomRate_url));
+        $this->waitForElement('#tab_0', 15000, 'css')->click();
+        $add_new_rate_plan = $this->waitForElement('#tab_0 .add_interval', 15000, 'css');
+        $add_new_rate_plan->click();
+        $this->byName('interval_name')->value($interval['name']);
+        $this->byName('start_date')->click();
+        $this->byCssSelector('.new_interval_form')->click();
+        $value = $this->convertDateToSiteFormat($interval['start']);
+        $this->byName('start_date')->value($value);
+        $this->byName('end_date')->click();
+        $this->byCssSelector('.new_interval_form')->click();
+        $value = $this->convertDateToSiteFormat($interval['end']);
+        $this->byName('end_date')->clear();
+        $this->byName('end_date')->value($value);
+        $this->byCssSelector('.new_interval_form')->click();
+
+        if (isset($interval['min'])){
+            $this->byName('min_los')->value($interval['min']);
+        }
+
+        if (isset($interval['max'])){
+            $this->byName('max_los')->value($interval['max']);
+        }
+
+        $l = $this->execute(array('script' => "return window.$('#tab_0 .define_week_days td:not(._hide) input').length", 'args' => array()));
+        for($i=0;$i<$l;$i++){
+            $el = $this->byJQ("#tab_0 .define_week_days td:not(._hide) input:eq(".$i.")");
+            $el->clear();
+            $el->value($interval['value_today']);
+        }
+
+
+        $this->byCssSelector('.new_interval_form a.save_add_interval')->click();
+
+        $save = $this->waitForElement('#panel-save .btn-save', 15000, 'css');
+        $save->click();
+        $this->waitForElement('.toast-bottom-left', 50000, 'css');
     }
+
+    public function rates_remove_rate() {
+        $this->url($this->_prepareUrl($this->roomRate_url));
+        $this->waitForLocation($this->_prepareUrl($this->roomRate_url));
+        $this->waitForElement('.nav-tabs .base.active a', 15000, 'css')->click();
+        $this->byJQ('.tab-pane.base.active .intervals-table tr.r_rate:last .interval_delete')->click();
+        $this->waitForElement('#confirm_delete', 50000, 'css');
+        $this->byCssSelector('#confirm_delete .btn_delete')->click();
+        $save = $this->waitForElement('#panel-save .btn-save', 15000, 'css');
+        $save->click();
+        $this->waitForElement('.toast-bottom-left', 50000, 'css');
+    }
+
 }
--- convert/derived_plan_should_show_additioinal_label_on_booking.php
+++ convert/derived_plan_should_show_additioinal_label_on_booking.php
@@ -1,38 +1,53 @@
 <?php
 namespace MyProject\Tests;
 require_once 'test_restrict.php';
+require_once 'common/rates.php';
 require_once 'common/packages.php';
 
 class derived_plan_should_show_additioinal_label_on_booking extends test_restrict {
-    use \Packages;
+    use \Rates, \Packages;
+
+    private $package = array(
+        '[name=\'package_name\']' => 'Derived Pack HOTELS-9974',
+        '[name=\'package_name_internal\']' => 'Derived Pack HOTELS-9974',
+        'is_derived' => true,//[name=\'derived\']
+        '.action_rate' => '-',
+        '.currency_rate' => 'percentage',
+        '[name=\'derived_rate\']' => 5,
+        'have_promo' => false,//[name=\'have_promo\']
+        '[id^=\'packages_descr_\']' => 'Nothing include. Just test package',
+        '[id^=\'packages_wysiwyg_terms_\']' => 'Has no any policy.',
+        'ranges' => array(
+            array(
+                'interval_name' => '(test percentage Derived)',
+                'end_date' => '+140 days',
+                'start_date' => '+121 days',
+                'min_los' => 0,
+                'max_los' => 0,
+                'cut_off' => 0,
+                'last_minute_booking' => 0,
+                'closed_to_arrival'  => false
+            )
+        )
+    );
+    private $interval = array(
+        'name' => 'interval today',
+        'value_today' => '99',
+        'end' => '+140 days',
+        'start' => '+121 days',
+        'min' => '2',
+        'edit_end_day' => '+12 days'
+    );
+
+
+
+
     public function testSteps() {
         $this->setupInfo('wwwdev3.ondeficar.com', '', '', 366);
         $this->loginToSite();
-        $package = array(
-            '[name=\'package_name\']' => 'Derived Pack HOTELS-9974',
-            '[name=\'package_name_internal\']' => 'Derived Pack HOTELS-9974',
-            'is_derived' => true,//[name=\'derived\']
-            '.action_rate' => '-',
-            '.currency_rate' => 'percentage',
-            '[name=\'derived_rate\']' => 5,
-            'have_promo' => false,//[name=\'have_promo\']
-            '[id^=\'packages_descr_\']' => 'Nothing include. Just test package',
-            '[id^=\'packages_wysiwyg_terms_\']' => 'Has no any policy.',
-            'ranges' => array(
-                array(
-                    'interval_name' => '(test percentage Derived)',
-                    'end_date' => '+140 days',
-                    'start_date' => '+121 days',
-                    'min_los' => 0,
-                    'max_los' => 0,
-                    'cut_off' => 0,
-                    'last_minute_booking' => 0,
-                    'closed_to_arrival'  => false
-                )
-            )
-        );
-
-        $package_id = $this->packages_add_package($package);
+        $interval_id = $this->rates_add_rate($this->interval);
+        $package_id = $this->packages_add_package($this->package);
         $this->packages_remove_package($package_id);
+        $this->rates_remove_rate();
     }
 }
