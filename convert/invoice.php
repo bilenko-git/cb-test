73f24dfd589a62a0279cc061f817ac7b62454c89
philipp <fil.jlh@bk.ru>
2015-09-25 14:40:35 +0300

    HOTELS-9998 Fix selenium test /packages_availability.php

--- convert/invoice.php
+++ convert/invoice.php
@@ -2,7 +2,7 @@
 namespace MyProject\Tests;
 require_once 'base_rates.php';
 
-class fAdd_min_max_rate extends test_restrict{
+class invoice extends test_restrict{
     private $invoice_url = 'http://{server}/connect/{property_id}#/setupInvoicing';
     private $reservas_url = 'http://{server}/reservas/{property_id}';
     private $availability_url = 'http://{server}/connect/{property_id}#/availability';
@@ -12,8 +12,12 @@ class fAdd_min_max_rate extends test_restrict{
         $this->url($this->_prepareUrl($this->invoice_url));
         $this->waitForLocation($this->_prepareUrl($this->invoice_url));
         $this->waitForElement('.invoice_preview', 15000, 'css')->click();
-        $this->waitForElement('.loading', 15000, 'css');
-        $this->waitForElement('.loading.hide', 15000, 'css');
+        $this->waitForElement('#layout_container>.loading:not(.hide)', 15000, 'jQ');
+
+        $this->keysSpecial('escape');$this->keysSpecial('escape');$this->keysSpecial('escape');
+        $this->waitForElement('#layout_container>.loading.hide', 15000, 'css');
+      //  $this->keyDown('theTextbox', '98');
+        $this->keysSpecial('escape');
     }
 
 }
