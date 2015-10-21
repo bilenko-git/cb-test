7e29f4e275909476ea64a6026f2eecd30cf4a106
philipp <fil.jlh@bk.ru>
2015-10-21 18:40:37 +0300

    rewrite old tests

--- convert/hotel_profile_ciry_error.php
+++ convert/hotel_profile_ciry_error.php
@@ -1,132 +1,80 @@
 <?php
-
 namespace MyProject\Tests;
+require_once 'test_restrict.php';
 
-use Sauce\Sausage\WebDriverTestCase;
+class add_interval_date_error extends test_restrict
+{
+    private $roomrate_url = 'http://{server}/connect/{property_id}#/roomRates';
 
-class hotel_profile_ciry_error extends WebDriverTestCase {
-    public static $browsers = array(
-        // run FF15 on Windows 8 on Sauce
-        array(
-            'browserName' => 'firefox',
-            'desiredCapabilities' => array(
-                'version' => '37',
-                'platform' => 'Windows 8.1',
-            )
-        ),
-        // run Chrome on Linux on Sauce
-       /* array(
+    public function testSteps()
+    {
+        $br = array(
             'browserName' => 'chrome',
             'desiredCapabilities' => array(
                 'platform' => 'Windows 8.1',
-            )
-        ),*/
-    );
-    /**
-     * Recorded steps.
-     */
-    public function testSteps() {
-        $test = $this; // Workaround for anonymous function scopes in PHP < v5.4.
-        $session = $this->prepareSession(); // Make the session available.
-        // get
-        $this->url("http://wwwdev.ondeficar.com/auth/login");
-        // setElementText
-        $element = $this->byId("email");
-        $element->click();
-        $element->clear();
-        $element->value("selenium@cloudbeds.com");
-        // setElementText
-        $element = $this->byId("password");
-        $element->click();
-        $element->clear();
-        $element->value("testTime!");
-        // clickElement
-        $this->byXPath("//div[@class='form-actions']//button[normalize-space(.)='Login']")->click();
-        // waitForCurrentUrl
-        $this->waitUntil(function() use ($test) {
-            try {
-                $boolean = ($test->byId("main_menu") instanceof \PHPUnit_Extensions_Selenium2TestCase_Element);
-            } catch (\Exception $e) {
-                $boolean = false;
-            }
-            return $boolean === true ?: null;
-        },50000);
+            ));
+        //$this->setupInfo('', '', '', 366, $br);
+        $this->loginToSite();
         // waitForEval
-        $this->waitUntil(function() use ($test) {
-            try {
-                $test->assertEquals("0", $test->execute(array('script' => "return window.$('body > .progress-bar-background:visible').length", 'args' => array())));
-            } catch(\Exception $e) {
-                return null;
-            }
-            return true;
-        },50000);
-        // waitForElementPresent
-        $this->waitUntil(function() use ($test) {
-            try {
-                $boolean = ($test->byCssSelector("#layout[data-current_view=dashboard]") instanceof \PHPUnit_Extensions_Selenium2TestCase_Element);
-            } catch (\Exception $e) {
-                $boolean = false;
-            }
-            return $boolean === true ?: null;
-        },50000);
-        // assertElementPresent
-        try {
-            $boolean = ($test->byId("main_menu") instanceof \PHPUnit_Extensions_Selenium2TestCase_Element);
-        } catch (\Exception $e) {
-            $boolean = false;
-        }
-        $test->assertTrue($boolean);
-    // clickElement
-    $this->byCssSelector("#ssettings > a")->click();
-    // clickElement
+        // clickElement
+        $this->byName("arates")->click();
+        // clickElement
+        $this->byCssSelector("#main_menu #sroomRates a")->click();
+        // waitForElementAttribute
+        $this->url($this->_prepareUrl($this->roomrate_url));
+        $this->waitForLocation($this->_prepareUrl($this->roomrate_url));
+        // clickElement
+        $this->byCssSelector("#ssettings > a")->click();
+        // clickElement
         $this->url("http://wwwdev.ondeficar.com/connect/366#/profileHotel");
-    //$this->byCssSelector("#sprofile1 > a")->click();
-    // clickElement
-    $this->byCssSelector("a[data-tutorial='propertyProfile']")->click();
-    // clickElement
-    // setElementText
-    $element = $this->byId("hotel_name");
-    $element->click();
-    $element->clear();
-    $element->value("SeleniumTest Hotel");
-    // setElementText
-    $element = $this->byId("hotel_phone");
-    $element->click();
-    $element->clear();
-    $element->value("(111)555-8899");
-    // setElementText
-    $element = $this->byId("hotel_contact_name");
-    $element->click();
-    $element->clear();
-    $element->value("Selenium hotelier");
-    // setElementText
-    $element = $this->byId("hotel_address1");
-    $element->click();
-    $element->clear();
-    $element->value("1 selenium st. Good City");
-    // setElementText
-    $element = $this->byId("hotel_address_city");
-    $element->click();
-    $element->clear();
-    $element->value("C");
-    // setElementText
-    $element = $this->byId("hotel_address_zip");
-    $element->click();
-    $element->clear();
-    $element->value("123789");
-    // clickElement
-    $this->byXPath("//div[@class='pull-line-right']//a[.=' Save']")->click();
-    // waitForText
+        //$this->byCssSelector("#sprofile1 > a")->click();
+        // clickElement
+        $this->byCssSelector("a[data-tutorial='propertyProfile']")->click();
+        // clickElement
+        // setElementText
+        $element = $this->byId("hotel_name");
+        $element->click();
+        $element->clear();
+        $element->value("SeleniumTest Hotel");
+        // setElementText
+        $element = $this->byId("hotel_phone");
+        $element->click();
+        $element->clear();
+        $element->value("(111)555-8899");
+        // setElementText
+        $element = $this->byId("hotel_contact_name");
+        $element->click();
+        $element->clear();
+        $element->value("Selenium hotelier");
+        // setElementText
+        $element = $this->byId("hotel_address1");
+        $element->click();
+        $element->clear();
+        $element->value("1 selenium st. Good City");
+        // setElementText
+        $element = $this->byId("hotel_address_city");
+        $element->click();
+        $element->clear();
+        $element->value("C");
+        // setElementText
+        $element = $this->byId("hotel_address_zip");
+        $element->click();
+        $element->clear();
+        $element->value("123789");
+        // clickElement
+        $save = $this->waitForElement('#panel-save .btn-save', 15000, 'css');
+        $save->click();
+        $this->waitForElement('.toast-bottom-left', 50000, 'css');
+        $this->waitForElement("#error_modal", 15000, 'css');
+        $this->waitForElement("#error_modal .default", 15000, 'css')->click();
 
-        $this->waitUntil(function() use ($test) {
-    try {
-        $boolean = ($test->byCssSelector(".validation_error[aria-hidden='false']") instanceof \PHPUnit_Extensions_Selenium2TestCase_Element);
-    } catch (\Exception $e) {
-        $boolean = false;
-    }
-            return true;
-        },50000);
-        $test->assertTrue($boolean);
-    // clickElement
+        $element = $this->byId("hotel_address_city");
+        $element->click();
+        $element->clear();
+        $element->value("Cccc");
+
+        $save = $this->waitForElement('#panel-save .btn-save', 15000, 'css');
+        $save->click();
+        $this->waitForElement('.toast-bottom-left', 50000, 'css');
     }
 }
