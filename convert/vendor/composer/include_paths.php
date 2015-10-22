c107261e96e346ce0490057120f56ead17a4b764
vitaly <vitaly@vitaly-VirtualBox>
2015-10-22 23:42:47 +0300

    HOTELS-11050 composer.phar update

--- convert/vendor/composer/include_paths.php
+++ convert/vendor/composer/include_paths.php
@@ -6,8 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    $vendorDir . '/phpunit/php-text-template',
-    $vendorDir . '/phpunit/php-timer',
     $vendorDir . '/phpunit/phpunit-selenium',
     $vendorDir . '/appium/php-client',
 );
