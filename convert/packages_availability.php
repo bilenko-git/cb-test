8d717020c11c857ba608a2e810b1b0c160f12035
philipp <fil.jlh@bk.ru>
2015-11-06 16:08:25 +0200

    additional_payment_methods.php

--- convert/packages_availability.php
+++ convert/packages_availability.php
@@ -202,7 +202,7 @@ class packages_availability extends test_restrict{
         )
     );
 
-  /*  public function test_Range_Min_Max_los(){
+    public function test_Range_Min_Max_los(){
         $this->go_to_package_page();
         $this->_verifyPackage(0);
     }
@@ -221,7 +221,7 @@ class packages_availability extends test_restrict{
       public function test_Promo_code(){
           $this->go_to_package_page();
           $this->_verifyPackage(4);
-      }*/
+      }
       public function test_Derived_fixed_package(){
           $this->go_to_package_page();
           $this->_verifyPackage(5);
@@ -230,10 +230,10 @@ class packages_availability extends test_restrict{
           $this->go_to_package_page();
           $this->_verifyPackage(6);
       }
-   /*   public function test_Package_update(){
+      public function test_Package_update(){
           $this->go_to_package_page();
           $this->_update_and_verifyPackage(0);
-      }*/
+      }
 
     public function _update_and_verifyPackage($index){
         if(!empty($this->packages[$index])) {
