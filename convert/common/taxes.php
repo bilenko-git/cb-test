adadea2d81cefde3ff7e3e6bd275e9bf37cd96b1
Dima Zabrodin <zabrodin17081990@gmail.com>
2016-02-16 18:55:45 +0300

    HOTELS-12738

--- convert/common/taxes.php
+++ convert/common/taxes.php
@@ -1,9 +1,74 @@
 <?php
-
 trait Taxes {
-    private $taxes_url = 'http://{server}/connect/{property_id}#/fees_and_taxes';
+    private $fees_and_taxes_url = 'http://{server}/connect/{property_id}#/fees_and_taxes';
+    private $reservation_url = 'http://{server}/connect/{property_id}#/newreservation';
+    private $transactions_url = 'http://{server}/connect/{property_id}#/report_transactions';
 
-    public function taxes_create_tax($tax) {
+    private function add_fee($fee) {
+        $this->url($this->_prepareUrl($this->fees_and_taxes_url));
+        $this->waitForLocation($this->_prepareUrl($this->fees_and_taxes_url));
+        $this->waitForElement('#layout .tabs_payments a', 15000, 'css');
+        $this->byJQ('#layout .tabs_payments a:first')->click();
+        $this->waitForElement('#layout .add-new-fee', 15000, 'css')->click();
+        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_name_en')->click();
+        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_name_en')->value($fee['name']);
+        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_amount')->click();
+        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_amount')->clear();
+        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_amount')->value($fee['amount']);
+        $this->waitForElement('.submit-fee', 15000, 'css')->click();
+        $this->waitForElement('.toast-close-button', 15000, 'css')->click();
+    }
+
+    private function add_tax($tax) {
+        $this->url($this->_prepareUrl($this->fees_and_taxes_url));
+        $this->waitForLocation($this->_prepareUrl($this->fees_and_taxes_url));
+        $this->byJQ('#layout .tabs_payments a:last')->click();
+        $this->waitForElement('#layout .add-new-tax', 15000, 'css')->click();
+        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_name_en')->click();
+        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_name_en')->value($tax['name']);
+        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_amount')->click();
+        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_amount')->clear();
+        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_amount')->value($tax['amount']);
+        $this->waitForElement('.submit-tax', 15000, 'css')->click();
+        $this->waitForElement('.toast-close-button', 15000, 'css')->click();
+    }
+
+    private function change_fee_name($fee) {
+        $this->execute(array('script' => "return BET.navigation.url('fees_and_taxes');", 'args' => array()));
+        $this->waitForElement('#layout .tabs_payments a', 15000, 'css');
+        $this->byJQ('#layout .tabs_payments a:first')->click();
+        $this->waitForElement('#layout .edit-fee', 15000, 'css')->click();
+        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_name_en')->click();
+        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_name_en')->clear();
+        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_name_en')->value($fee['name_changed']);
+        $this->waitForElement('.submit-fee', 15000, 'css')->click();
+        $this->waitForElement('.toast-close-button', 15000, 'css')->click();
         return false;
     }
+
+    private function change_tax_name($tax) {
+        $this->execute(array('script' => "return BET.navigation.url('fees_and_taxes');", 'args' => array()));
+        $this->waitForElement('#layout .tabs_payments a', 15000, 'css');
+        $this->byJQ('#layout .tabs_payments a:last')->click();
+        $this->waitForElement('#layout .edit-tax', 15000, 'css')->click();
+        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_name_en')->click();
+        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_name_en')->clear();
+        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_name_en')->value($tax['name_changed']);
+        $this->waitForElement('.submit-tax', 15000, 'css')->click();
+        $this->waitForElement('.toast-close-button', 15000, 'css')->click();
+        return false;
+    }
+
+    private function remove_fee() {
+        $this->byJQ('#layout .tabs_payments a:first')->click();
+        $this->waitForElement('#layout .delete-fee', 15000, 'css')->click();
+        $this->waitForElement('#confirm_delete .btn_delete', 15000, 'css')->click();
+    }
+
+    private function remove_tax() {
+        $this->byJQ('#layout .tabs_payments a:last')->click();
+        $this->waitForElement('#layout .delete-tax', 15000, 'css')->click();
+        $this->waitForElement('#confirm_delete .btn_delete', 15000, 'css')->click();
+    }
+
 }
--- convert/tax_rename_change_previous_transactions.php
+++ convert/tax_rename_change_previous_transactions.php
@@ -1,11 +1,11 @@
 <?php
 namespace MyProject\Tests;
 require_once 'test_restrict.php';
+require_once 'common/taxes.php';
 
 class tax_rename_change_previous_transactions extends test_restrict {
-    private $fees_and_taxes_url = 'http://{server}/connect/{property_id}#/fees_and_taxes';
-    private $reservation_url = 'http://{server}/connect/{property_id}#/newreservation';
-    private $transactions_url = 'http://{server}/connect/{property_id}#/report_transactions';
+    use \Taxes;
+
     private $fee = array(
         'name' => 'Fee Transactions #1',
         'name_changed' => 'Fee Transactions #1 changed',
@@ -21,11 +21,10 @@ class tax_rename_change_previous_transactions extends test_restrict {
         //$this->setupInfo('wwwdev3.ondeficar.com', '', '', 366);
         $this->setupInfo('PMS_user');
         $this->loginToSite();
-
         $this->prepare_data();
-        $this->change_fee_name();
+        $this->change_fee_name($this->fee);
         $this->check_transactions();
-        $this->change_tax_name();
+        $this->change_tax_name($this->tax);
         $this->check_transactions();
         $this->add_adjustments();
         $this->check_transactions();
@@ -33,46 +32,18 @@ class tax_rename_change_previous_transactions extends test_restrict {
     }
 
     private function prepare_data() {
-        $this->add_fee();
-        $this->add_tax();
+        $this->add_fee($this->fee);
+        $this->add_tax($this->tax);
         $this->add_reservation();
         $this->add_transactions();
     }
 
     private function clear_data() {
         $this->remove_fee();
+        $this->waitForElement('#layout .tabs_payments a', 10000, 'css')->click();
         $this->remove_tax();
         $this->remove_reservation();
         $this->remove_transactions();
-    }
-
-    private function add_fee() {
-        $this->url($this->_prepareUrl($this->fees_and_taxes_url));
-        $this->waitForLocation($this->_prepareUrl($this->fees_and_taxes_url));
-        $this->waitForElement('#layout .tabs_payments a', 15000, 'css');
-        $this->byJQ('#layout .tabs_payments a:first')->click();
-        $this->waitForElement('#layout .add-new-fee', 15000, 'css')->click();
-        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_name_en')->click();
-        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_name_en')->value($this->fee['name']);
-        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_amount')->click();
-        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_amount')->clear();
-        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_amount')->value($this->fee['amount']);
-        $this->waitForElement('.submit-fee', 15000, 'css')->click();
-        $this->waitForElement('.toast-close-button', 15000, 'css')->click();
-    }
-
-    private function add_tax() {
-        $this->url($this->_prepareUrl($this->fees_and_taxes_url));
-        $this->waitForLocation($this->_prepareUrl($this->fees_and_taxes_url));
-        $this->byJQ('#layout .tabs_payments a:last')->click();
-        $this->waitForElement('#layout .add-new-tax', 15000, 'css')->click();
-        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_name_en')->click();
-        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_name_en')->value($this->tax['name']);
-        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_amount')->click();
-        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_amount')->clear();
-        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_amount')->value($this->tax['amount']);
-        $this->waitForElement('.submit-tax', 15000, 'css')->click();
-        $this->waitForElement('.toast-close-button', 15000, 'css')->click();
     }
 
     private function add_reservation() {
@@ -86,51 +57,12 @@ class tax_rename_change_previous_transactions extends test_restrict {
         return false;
     }
 
-    private function change_fee_name() {
-        $this->execute(array('script' => "return BET.navigation.url('fees_and_taxes');", 'args' => array()));
-        $this->waitForElement('#layout .tabs_payments a', 15000, 'css');
-        $this->byJQ('#layout .tabs_payments a:first')->click();
-        $this->waitForElement('#layout .edit-fee', 15000, 'css')->click();
-        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_name_en')->click();
-        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_name_en')->clear();
-        $this->byJQ('#layout .add-fee-portlet-box:not(.clonable) #fee_name_en')->value($this->fee['name_changed']);
-        $this->waitForElement('.submit-fee', 15000, 'css')->click();
-        $this->waitForElement('.toast-close-button', 15000, 'css')->click();
-        return false;
-    }
-
-    private function change_tax_name() {
-        $this->execute(array('script' => "return BET.navigation.url('fees_and_taxes');", 'args' => array()));
-        $this->waitForElement('#layout .tabs_payments a', 15000, 'css');
-        $this->byJQ('#layout .tabs_payments a:last')->click();
-        $this->waitForElement('#layout .edit-tax', 15000, 'css')->click();
-        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_name_en')->click();
-        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_name_en')->clear();
-        $this->byJQ('#layout .add-tax-portlet-box:not(.clonable) #tax_name_en')->value($this->tax['name_changed']);
-        $this->waitForElement('.submit-tax', 15000, 'css')->click();
-        $this->waitForElement('.toast-close-button', 15000, 'css')->click();
-        return false;
-    }
-
     private function check_transactions() {
         return false;
     }
 
     private function add_adjustments() {
         return false;
-    }
-
-    private function remove_fee() {
-        $this->byJQ('#layout .tabs_payments a:first')->click();
-        $this->waitForElement('#layout .delete-fee', 15000, 'css')->click();
-        $this->waitForElement('#confirm_delete .btn_delete', 15000, 'css')->click();
-    }
-
-    private function remove_tax() {
-        $this->waitForElement('#layout .tabs_payments a', 10000, 'css')->click();
-        $this->byJQ('#layout .tabs_payments a:last')->click();
-        $this->waitForElement('#layout .delete-tax', 15000, 'css')->click();
-        $this->waitForElement('#confirm_delete .btn_delete', 15000, 'css')->click();
     }
 
     private function remove_reservation() {
