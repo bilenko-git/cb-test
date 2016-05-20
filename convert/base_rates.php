9b358043b483ae02e47c8078580664495be56783
philipp <fil.jlh@bk.ru>
2016-05-20 12:25:43 +0300

    fix unclickable el

--- convert/base_rates.php
+++ convert/base_rates.php
@@ -157,7 +157,7 @@ class base_rates extends test_restrict{
             $this->waitForElement('#layout .intervals-table tr.r_rate:last .interval_edit', 15000, 'jQ')->click();
         } else {
             //$this->waitForElement('.nav-tabs li.base:first a', 15000, 'jQ')->click();
-           // $this->waitForElement('#tab_0 .intervals-table tr.r_rate:last .interval_edit', 15000, 'jQ')->click();
+            $this->waitForElement('#layout .intervals-table tr.r_rate:last .interval_edit', 15000, 'jQ')->click();
         }
         $this->waitForElement('[name=interval_name]', 15000, 'jQ' )->value($interval['name']);
         $this->waitForElement('[name=start_date]', 15000, 'jQ')->click();
